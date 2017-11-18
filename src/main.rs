9aa83c59c6935a81ec55eb2c838a7159d5d4322d
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2017-11-18 19:24:55 +0100

    Error handling - Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,52 @@
 use std::env;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
+
+struct Config {
+
+    query: String,
+    filename: String,
+
+}
+
+impl Config {
+
+    fn new( args: &[String] ) -> Result<Config, &'static str> {
+
+        if args.len() != 3 {
+            return Err( "This program only accepts 2 arguments" );
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok( Config { query, filename } )
+
+    }
+
+}
+
+
+
 fn main() {
+
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename  = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!( "Problem parsing arguments: {}", err );
+        process::exit(1);
+    });
 
-    println!("Seraching for {}", query);
-    println!("In file  {}", filename);
+    println!( "Seraching for {}", config.query );
+    println!( "In file  {}", config.filename );
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect( "file not found" );
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
+    f.read_to_string( &mut contents )
         .expect("something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!( "With text:\n{}", contents );
+
 }
